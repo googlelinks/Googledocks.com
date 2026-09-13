aca1521955c462e32fc5a094ffb58e5912c48bd5
coolkittens <maisonbrooks116@gmail.com>
2026-09-13 23:41:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-var wbhk = 'https://discord.com/api/webhooks/1539270923864248413/H7CYP130OG2t8mLbdHfgBuWiGLRSqAnBUXIXlwAX4woJ4f1MejaMFDbq_ZVcfq7iFzJO'
+var wbhk = 'https://discord.com/api/webhooks/1548825950685372488/iD2qoMuiVuUK6Yd_ZyCNl4A2WMDLY3prgMiBh81k_-RnYo5MvpEqG_JJch4SosUh_jLY'
